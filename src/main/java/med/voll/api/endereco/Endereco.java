b82b163aab3c5ec7422e65c24edef414b9337434
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-02-27 17:57:13 -0300

    crir a classe Medico e mapear com a JPA

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,24 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.awt.*;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+}
