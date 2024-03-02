623e01c555589e069eb036b89d3be0410f1f267b
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-03-02 19:54:53 -0300

    fazer a função de recuperar token recebido

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -15,7 +15,17 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJwt = recuperarToken(request);
+        System.out.println(tokenJwt);
 
         filterChain.doFilter(request, response);//chamando o próximo filtro
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var autorizationHeader = request.getHeader("Authorization");
+        if(autorizationHeader == null) {
+            throw new RuntimeException("Token não enviado");
+        }
+        return autorizationHeader.replace("Bearer ", "");
     }
 }
